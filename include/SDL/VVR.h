59f5e032728ba1436b24b088d2ffa8fc83130038
Nathan Labbé <60106942+Bouierck@users.noreply.github.com>
2021-04-10 16:18:42 +0200

    Add files via upload

---
+++ include/SDL/VVR.h
@@ -0,0 +1,11 @@
+//! 
+
+void quitter();
+int VVR_SDL();
+
+extern SDL_Renderer* renderer;
+extern int leftMouseDown;
+extern Mix_Chunk* ButtonHoverSFX;
+extern Mix_Chunk* ButtonClickedSFX;
+extern Mix_Chunk* ButtonBackSFX;
+extern int ActiveMenu;
